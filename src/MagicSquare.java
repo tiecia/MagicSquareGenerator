b0a167ddbcce484c143ff9c2678c2dfc71949ded
VinnyVinVince <thevinnycast@gmail.com>
2020-02-09 15:15:02 -0800

    whitespace

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -1,24 +1,43 @@
 public class MagicSquare {
 
     public static void printAllBoards(int size){
+
         explore(0, new Board(size));
+
     }
 
-    public static void explore(int col, Board board){;
+    public static void explore(int col, Board board) {
+
         System.out.println("explore");
-        if(col == board.size()){
+
+        if (col == board.size()) {
+
             board.printBoard();
+
         } else {
-            for(int i = 0; i<board.size(); i++){ //Go through each row in the column
-                for(int n = 1; n<Math.pow(board.size(), 2); n++){
-                    if(col == board.size()-1 && board.checkValid(i, col, n)) {
+
+            for (int i = 0; i < board.size(); i++){ //Go through each row in the column
+
+                for (int n = 1; n <= Math.pow(board.size(), 2); n++) {
+
+                    if (col == board.size()-1 && board.checkValid(i, col, n)) {
+
                         explore(col+1, board);
-                    } else if(board.checkValid(i, col, n)){
+
+                    } else if (board.checkValid(i, col, n)){
+
                         explore(col, board);
+
                     }
+
                     board.remove(i, col);
+
                 }
+
             }
+
         }
+
     }
+
 }
