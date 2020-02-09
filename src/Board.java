142c6f5a5b6f70b666c83e6b4e62b3b4aa803937
Tie C <usbtyping@gmail.com>
2020-02-09 13:12:14 -0800

    Work on Recursion

--- src/Board.java
+++ src/Board.java
@@ -1,11 +1,7 @@
-import com.sun.javaws.exceptions.InvalidArgumentException;
-
-import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class Board {
 
-    private int size;
     private int sum;
 
     private int[][] board;
@@ -14,7 +10,6 @@ public class Board {
         if(size < 3){
             throw new IllegalArgumentException("Size Too Small");
         }
-        this.size = size;
         this.sum = (int)(Math.pow(size, 3) + size)/2;
         this.board = new int[size][size];
     }
@@ -23,55 +18,102 @@ public class Board {
         if(board[row][col] != 0){
             return false;
         }
-        for(int[] rowList : board) {
-            int[] sorted = rowList.clone();
-            Arrays.sort(sorted);
-            System.out.println(Arrays.toString(sorted));
-            if (Arrays.binarySearch(sorted, num) < 0) {
-                return false;
-            }
-        }
-        if(row == col && !checkDiag()){
+        if(contains(num)){
             return false;
         }
-        if(!checkRow(row) || !checkColumn(col)){
+        if(!checkNegDiag(row, col, num) || !checkPosDiag(row, col, num)){
+            return false;
+        }
+        if(!isRowValid(row, col, num) || !isColumnValid(row, col, num)){
             return false;
         }
         board[row][col] = num;
         return true;
     }
 
-//    public void place(int row, int col, int num, int nu1){
-//        board[row][col] = num;
-//    }
+    public void place(int row, int col, int num){
+        board[row][col] = num;
+    }
 
-    private boolean checkRow(int row){
+    private boolean contains(int num){
+        for(int[] arr : board){
+            for(int current : arr){
+                if(num == current){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isRowValid(int row, int col, int num){
         int sum = 0;
-        for(int num : board[row]){
-            sum += num;
+        for(int currentNum : board[row]) {
+            sum += currentNum;
+        }
+        sum += num;
+        if(col == board.length-1 && sum != this.sum ) { //Dead end for row not adding up
+            return false;
         }
         return sum <= this.sum;
     }
 
-    private boolean checkColumn(int col){
-        return true;
+    private boolean isColumnValid(int row, int col, int num){
+        int sum = 0;
+        for(int[] arr : board){
+            sum += arr[col];
+        }
+        sum+= num;
+        if(row == board.length-1 && sum != this.sum){ //Dead end for column not adding up
+            return false;
+        }
+        return sum <= this.sum;
     }
 
-    private boolean checkDiag(){
-        return true;
+    //Return true if number can be placed there or if input is not on a diag
+    //Return false is number cannot be placed there
+    private boolean checkNegDiag(int row, int col, int num){
+        if(row != col){
+            return true;
+        }
+        int sum = 0;
+        for(int i = 0; i<board.length; i++){
+            sum += board[i][i];
+        }
+        if(row == board.length-1 && sum+num != this.sum){
+            return false;
+        }
+        return sum <= this.sum;
     }
 
-    public void place(int row, int col, int num){
-
+    private boolean checkPosDiag(int row, int col, int num){
+        if(row+col != board.length){ //Dead end for if point is on of this diag
+            return true;
+        }
+        int sum = 0;
+        int n = board.length-1;
+        for(int i = 0; i<board.length; i++){
+            sum += board[i][n];
+            if(row == 0 && sum+num != this.sum){  //TODO if number is placed in row 0 this will always be triggered off.
+                return false;
+            }
+            n--;
+        }
+        return sum <= this.sum;
     }
 
     public void remove(int row, int col){
         board[row][col] = 0;
     }
 
+    public int size(){
+        return board.length;
+    }
+
     public void printBoard(){
         for(int[] list : board){
             System.out.println(Arrays.toString(list));
         }
+        System.out.println();
     }
 }
--- src/ClientTest.java
+++ src/ClientTest.java
@@ -3,10 +3,18 @@ import java.util.Arrays;
 public class ClientTest {
 
     public static void main(String[] args) {
-        Board b = new Board(3);
-        System.out.println(b.checkValid(0, 0, 1));
-        System.out.println(b.checkValid(1, 0, 2));
-        System.out.println(b.checkValid(1, 1, 2));
-        b.printBoard();
+//        Board b = new Board(3);
+//        System.out.println(b.checkValid(0, 0, 4));
+//        System.out.println(b.checkValid(1, 0, 3));
+//        System.out.println(b.checkValid(2, 0, 8));
+//        System.out.println(b.checkValid(0, 1, 9));
+//        System.out.println(b.checkValid(1, 1, 5));
+//        System.out.println(b.checkValid(2, 1, 1));
+//        System.out.println(b.checkValid(0, 2, 2));
+//        System.out.println(b.checkValid(1, 2, 7));
+//        System.out.println(b.checkValid(2, 2, 6));
+//        b.printBoard();
+
+        MagicSquare.printAllBoards(3);
     }
 }
